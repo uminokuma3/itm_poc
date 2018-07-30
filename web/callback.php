b5c170966820564cd97635299b439e7fc600cd56
uminokuma3 <41911261+uminokuma3@users.noreply.github.com>
2018-07-30 23:34:21 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -135,7 +135,7 @@ if ($text == 'はい') {
     "altText" => "こんにちは 何かご用ですか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
-        "text" => "こんにちわ 何かご用ですか？",
+        "text" => "こんにちは 何かご用ですか？",
         "actions" => [
             [
               "type" => "message",
